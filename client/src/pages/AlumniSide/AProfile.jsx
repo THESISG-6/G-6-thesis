184005af69ff8c0c66f0bf9a9d1a3afd83894a51
nigels24 <nigesipe@gmail.com>
2023-11-02 15:57:05 +0700

    refactor-aprofile

--- client/src/pages/AlumniSide/AProfile.jsx
+++ client/src/pages/AlumniSide/AProfile.jsx
@@ -62,86 +62,59 @@ const AProfile = () => {
                       <label>Year Graduated</label>
                       <input type="text" placeholder="2022-2023" required />
                     </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="form second">
-                <div className="details personal">
-                  <div className="fields">
                     <div className="input-fields">
                       <label>Employment Status</label>
                       <input type="text" placeholder="Employed" />
                     </div>
+                    <div className="input-fields">
+                      <label>Current Job</label>
+                      <input type="text" placeholder="Military Officer" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Year(s) in Current Job</label>
+                      <input type="number" placeholder="5 " />
+                    </div>
+                    <div className="input-fields">
+                      <label>
+                        How long did you land a job after graduation?
+                      </label>
+                      <input type="text" placeholder="6 months - 1 year" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Position in Current Job</label>
+                      <input type="text" placeholder="2 Lt" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Employment Type</label>
+                      <input type="text" placeholder="Regular" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Employer/Place of Current Job </label>
+                      <input type="text" placeholder="Tabak 1ID" />
+                    </div>
 
-                    <>
-                      <div className="input-fields">
-                        <label>Current Job</label>
-                        <input type="text" placeholder="Military Officer" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Year(s) in Current Job</label>
-                        <input type="number" placeholder="5 " />
-                      </div>
-                      <div className="input-fields">
-                        <label>
-                          How long did you land a job after graduation?
-                        </label>
-                        <input type="text" placeholder="" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Position in Current Job</label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Position Status</label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Employer/Place of Current Job </label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Previous Job (if applicable) </label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Years in Previous Job (if applicable) </label>
-                        <input type="number" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Position in Previous Job(if applicable) </label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>
-                          Employer/Place of Previous Job(if applicable){" "}
-                        </label>
-                        <input type="text" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Engage in further Studies?</label>
+                    <div className="input-fields">
+                      <label>Engage in further Studies?</label>
 
-                        <input type="text" placeholder="" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Enroll Further Studies?</label>
-                        <input type="text" placeholder="" />
-                      </div>
-                      <div className="input-fields">
-                        <label>Eligibility Acquired (if any)</label>
-                        <input type="text" placeholder="" />
-                      </div>
-                    </>
-                  </div>
-
-                  <div className="buttons">
-                    <div className="backBtn">
-                      <i className="uil uil-navigator"></i>
-                      <span className="btnText">Update</span>
+                      <input type="text" placeholder="Yes" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Enroll Further Studies?</label>
+                      <input type="text" placeholder="Scout Ranger Schooling" />
+                    </div>
+                    <div className="input-fields">
+                      <label>Eligibility Acquired (if any)</label>
+                      <input type="text" placeholder="Milatary Officer Eligibility" />
                     </div>
                   </div>
                 </div>
+              </div>
+
+              <div className="buttons">
+                <div className="backBtn">
+                  <i className="uil uil-navigator"></i>
+                  <span className="btnText">Update</span>
+                </div>  
               </div>
             </form>
           </div>
