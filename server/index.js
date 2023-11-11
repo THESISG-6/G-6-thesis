dedc18782ec013da51f14af97b849a0744e8d35f
nigels24 <nigesipe@gmail.com>
2023-11-11 14:18:31 +0700

    [Task] Activity logs

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { applyMiddlewares } = require("./middlewares");
 const { dbConnect } = require("./configs/db");
 const { watch } = require("./configs/db-watcher");
-// const { watch } = require("./configs/db-watcher");
 const { routerMiddleware } = require("./middlewares/router");
 
 const app = express();
@@ -10,11 +9,11 @@ applyMiddlewares(app);
 routerMiddleware(app);
 
 app.listen(3001, () => {
-	Promise.resolve(dbConnect())
-		.then(() => console.log("Server is running on port 3001"))
-		.catch((e) => {
-			console.error("Unable to connect to the database", e);
-		});
+  Promise.resolve(dbConnect())
+    .then(() => console.log("Server is running on port 3001"))
+    .catch((e) => {
+      console.error("Unable to connect to the database", e);
+    });
 });
 
-// watch().then(() => console.log("Watching events..."));
+watch().then(() => console.log("Watching events..."));
